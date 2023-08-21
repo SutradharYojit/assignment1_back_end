757113282893acbaef7fa5851eca0ed9f272e629
SutradharYojit <jimmysuthar08@gmail.com>
2023-08-21 15:43:25 +0530

    add the changes

--- app.js
+++ app.js
@@ -6,7 +6,20 @@ const app = express();
 const bookRouts = require('./api/routes/products');
 
 mongoose.connect('mongodb+srv://jimmysuthar08:jimmysuthar08@learningrest.rdlntht.mongodb.net/?retryWrites=true&w=majority');
-app.use(morgan('dev'));// showing that which type of request is called
 app.use('/product', bookRouts);
 
+app.use((req, res, next) => {
+    const error = new Error('not found');
+    error.status = 404;
+    next(error)
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+});
 module.exports = app;
