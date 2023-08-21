9dff346e07096876ce3faad9f69b60ee380a7237
SutradharYojit <jimmysuthar08@gmail.com>
2023-08-21 15:58:59 +0530

    deploy bug fix

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,25 +5,22 @@ const mongoose = require('mongoose');
 const router = express();
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message:"service"
-    })
 
-    // Products.find().select("name price description author stock").exec().then(doc => {
-    //     console.log(doc);
-    //     if (doc == null) {
-    //         res.status(404).json({
-    //             message: "No products found"
-    //         });
-    //     }
-    //     else {
-    //         res.status(200).json({
-    //             count: doc.length,
-    //             bookItem: doc
-    //         });
+    Products.find().select("name price description author stock").exec().then(doc => {
+        console.log(doc);
+        if (doc == null) {
+            res.status(404).json({
+                message: "No products found"
+            });
+        }
+        else {
+            res.status(200).json({
+                count: doc.length,
+                bookItem: doc
+            });
 
-    //     }
-    // }).catch(err => console.log(err));
+        }
+    }).catch(err => console.log(err));
 });
 
 module.exports = router;
