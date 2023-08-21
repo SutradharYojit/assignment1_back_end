bb84c4307d30a03cf5e74838b6cf0a5c4de928e9
SutradharYojit <jimmysuthar08@gmail.com>
2023-08-21 15:57:53 +0530

    deploy bug fix

--- app.js
+++ app.js
@@ -7,18 +7,18 @@ const bookRouts = require('./api/routes/products');
 mongoose.connect('mongodb+srv://jimmysuthar08:jimmysuthar08@learningrest.rdlntht.mongodb.net/?retryWrites=true&w=majority');
 app.use('/product', bookRouts);
 
-app.use((req, res, next) => {
-    const error = new Error('not found');
-    error.status = 404;
-    next(error)
-});
+// app.use((req, res, next) => {
+//     const error = new Error('not found');
+//     error.status = 404;
+//     next(error)
+// });
 
-app.use((error, req, res, next) => {
-    res.status(error.status || 500)
-    res.json({
-        error: {
-            message: error.message
-        }
-    })
-});
+// app.use((error, req, res, next) => {
+//     res.status(error.status || 500)
+//     res.json({
+//         error: {
+//             message: error.message
+//         }
+//     })
+// });
 module.exports = app;
