6f3fb251f9eb17789ef158f40fc82c0d6e5912aa
SutradharYojit <jimmysuthar08@gmail.com>
2023-08-21 15:49:48 +0530

    add

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,21 +5,25 @@ const mongoose = require('mongoose');
 const router = express();
 
 router.get('/', (req, res, next) => {
-    Products.find().select("name price description author stock").exec().then(doc => {
-        console.log(doc);
-        if (doc == null) {
-            res.status(404).json({
-                message: "No products found"
-            });
-        }
-        else {
-            res.status(200).json({
-                count: doc.length,
-                bookItem: doc
-            });
+    res.status(200).json({
+        message:"service"
+    })
 
-        }
-    }).catch(err => console.log(err));
+    // Products.find().select("name price description author stock").exec().then(doc => {
+    //     console.log(doc);
+    //     if (doc == null) {
+    //         res.status(404).json({
+    //             message: "No products found"
+    //         });
+    //     }
+    //     else {
+    //         res.status(200).json({
+    //             count: doc.length,
+    //             bookItem: doc
+    //         });
+
+    //     }
+    // }).catch(err => console.log(err));
 });
 
 router.post('/', (req, res, next) => {
